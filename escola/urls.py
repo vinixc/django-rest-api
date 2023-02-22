bf63673b5efcf6e08ddfce9f23fec10dc8de5a88
Vinicius Carvalho <vinicius.carvalho>
2023-02-22 18:59:44 -0300

    configurando urls

--- escola/urls.py
+++ escola/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from escola.views import alunos
+from django.urls import path, include
+from escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
-    path('alunos/', alunos)
+    path('', include(router.urls))
 ]
