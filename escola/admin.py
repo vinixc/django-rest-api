f1a66237e4e18a2d9317e0f4fe4ec84bc662f081
Vinicius Carvalho <vinicius.carvalho>
2023-02-22 18:16:13 -0300

    criando models de aluno e curso e preparando admin para inserir dados

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from escola.models import Aluno,Curso
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ("id","nome",'rg','cpf','data_nascimento')
+    list_display_links = ('id','nome')
+    search_fields = ('nome',)
+    list_per_page = 10
+
+admin.site.register(Aluno, Alunos)
+
+
+class Cursos(admin.ModelAdmin):
+    list_display = ("id","codigo_curso",'descricao','nivel')
+    list_display_links = ('id','descricao')
+    search_fields = ('descricao',)
+    list_per_page = 10
+
+admin.site.register(Curso, Cursos)
